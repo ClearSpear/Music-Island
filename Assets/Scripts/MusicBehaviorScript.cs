07e5f34a6322128520f6f634954cacaf6fb5bfcd
Sicheng Zeng <sicheng.zeng.97@gmail.com>
2018-01-02 23:28:18 -0500

    adding project to github

---
+++ Assets/Scripts/MusicBehaviorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBehaviorScript : MonoBehaviour {
+
+    public GameObject audiosource;
+    public Material inactiveMaterial;
+
+    private GvrAudioSource gvrAS;
+
+    // Use this for initialization
+    void Start () {
+        gvrAS = audiosource.GetComponent<GvrAudioSource>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( !gvrAS.isPlaying )
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            GetComponent<Renderer>().material = inactiveMaterial;
+        }
+	}
+}
